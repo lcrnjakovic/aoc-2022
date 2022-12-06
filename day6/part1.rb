d74b1b19b4dbc54d6c57d3c08dabc498273e517f
Luka Crnjakovic <luka.crnjakovic@smartpension.co.uk>
2022-12-06 10:12:02 +0100

    day6

--- day6/part1.rb
+++ day6/part1.rb
@@ -1,2 +1,13 @@
+require 'pry'
+
+counter = 4
+
 File.open('input.txt').each do |line|
+  line.chars.each_cons(4) do |seq|
+    if seq == seq.uniq
+      p counter
+      return
+    end
+    counter += 1
+  end
 end
--- day6/part2.rb
+++ day6/part2.rb
@@ -1,2 +1,13 @@
+require 'pry'
+
+counter = 14
+
 File.open('input.txt').each do |line|
+  line.chars.each_cons(14) do |seq|
+    if seq == seq.uniq
+      p counter
+      return
+    end
+    counter += 1
+  end
 end
