fd3d80808833ad5c7eb87a654a129b7a4baabecd
lcrnjakovic <luka.crnjakovic@gmail.com>
2022-12-08 18:57:45 +0100

    day8

--- day8/part1.rb
+++ day8/part1.rb
@@ -1,2 +1,26 @@
-File.open('input.txt').each do |line|
+require 'pry'
+
+grid = Array.new(99) { Array.new(99) }
+visible = Array.new(99) { Array.new(99) }
+
+File.open('input.txt').each_with_index do |line, i|
+  grid[i] = line.strip.chars.map(&:to_i)
 end
+
+99.times do |x|
+  99.times do |y|
+    if x.zero? || x == 98 || y.zero? || y == 98
+      visible[x][y] = true
+    else
+      neighbors = []
+      neighbors << grid[x][0...y]
+      neighbors << grid[x][y+1..]
+      neighbors << grid.transpose[y][0...x]
+      neighbors << grid.transpose[y][x+1..]
+
+      visible[x][y] = neighbors.any? { |w| w.all? { |z| z < grid[x][y] }}
+    end
+  end
+end
+
+p visible.flatten.count(true)
--- day8/part2.rb
+++ day8/part2.rb
@@ -1,2 +1,27 @@
-File.open('input.txt').each do |line|
+require 'pry'
+
+grid = Array.new(99) { Array.new(99) }
+scores = Array.new(99) { Array.new(99) }
+
+File.open('input.txt').each_with_index do |line, i|
+  grid[i] = line.strip.chars.map(&:to_i)
 end
+
+99.times do |x|
+  99.times do |y|
+    neighbors = []
+    neighbors << grid[x][0...y]
+    neighbors << grid[x][y+1..]
+    neighbors << grid.transpose[y][0...x]
+    neighbors << grid.transpose[y][x+1..]
+
+    counts = neighbors.map.with_index do |arr, i|
+      arr = arr.reverse if i.zero? || i == 2
+      arr[0..arr.find_index { |w| w >= grid[x][y] }].count
+    end
+
+    scores[x][y] = counts.inject(:*)
+  end
+end
+
+p scores.flatten.max
